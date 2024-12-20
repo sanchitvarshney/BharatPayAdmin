47611a0db375dba0509b3ee431281bade06c3a63
kanishka-mscorpes <139351665+kanishka-mscorpes@users.noreply.github.com>
2024-12-20 11:39:31 +0530

    change 2

--- src/components/table/Notification/NotificationTable.tsx
+++ src/components/table/Notification/NotificationTable.tsx
@@ -26,12 +26,12 @@ interface MenuData {
   icon: string | null;
 }
 
-type Props = {
-  setViewMenu?: React.Dispatch<React.SetStateAction<boolean>>;
-  selectedType: string;
-  selectedVal: string;
-  updateRow: any;
-};
+// type Props = {
+//   setViewMenu?: React.Dispatch<React.SetStateAction<boolean>>;
+//   selectedType: string;
+//   selectedVal: string;
+//   updateRow: any;
+// };
 
 interface RowData {
   orgHierarchy: string[];
@@ -77,7 +77,7 @@ const flattenMenuHierarchy = (
   return result;
 };
 
-const NotificationTable: React.FC<Props> = ({}) => {
+const NotificationTable = () => {
   const gridRef = useRef<AgGridReact>(null);
   const [rowData, setRowData] = useState<RowData[]>([]);
   const { menuList } = useAppSelector((state) => state.menu);
--- src/pages/Notification/Notification.tsx
+++ src/pages/Notification/Notification.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import NotificationTable from "@/components/table/Notification/NotificationTable";
 const Notification = () => {
   return (
